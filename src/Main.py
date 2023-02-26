48f5ddd48e58c0917922e12d055305bb2de1991e
jprincipe6 <jeanpaul.principe@gmail.com>
2023-02-26 01:36:22 +0100

    feat: NearestNeighbor, first iteration

--- src/Main.py
+++ src/Main.py
@@ -2,9 +2,15 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+from NearestNeighbor import NearestNeighbor
+
 img = cv2.imread('../resources/aef-CSN-III-3-1_088-300x450.jpg')
 print(type(img))
-print(img.shape[0], img.shape[1])
+# print(img.shape[0], img.shape[1])
 
-cv2.imshow('Figure',img)
+cv2.imshow('Original Figure',img)
+# cv2.waitKey()
+
+resizedImage = NearestNeighbor.run(img)
+cv2.imshow('Figure resized',resizedImage)
 cv2.waitKey()
