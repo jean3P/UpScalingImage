b750faba786c0649851080d4e387fbb673d8cf85
jprincipe6 <jeanpaul.principe@gmail.com>
2023-03-02 21:40:12 +0100

    feat: assignment 2

--- src/Main_Signal_Noise_Ratio.py
+++ src/Main_Signal_Noise_Ratio.py
@@ -15,14 +15,15 @@ pathNearestNeighbour = '../resources/nearestNeighbour'
 originalImages = os.listdir(pathOriginalSize)
 nearestNeighbourImages = sorted(os.listdir(pathNearestNeighbour))
 
-def SNR(original, compressed):
-    mse = np.mean((original - compressed) ** 2)
-    if(mse == 0):  # MSE is zero means no noise is present in the signal .
-                  # Therefore PSNR have no importance.
-        return 100
-    max_pixel = 255.0
-    psnr = 20 * log10(max_pixel / sqrt(mse))
-    return psnr
+def signal_noise_ratio(original, compressed):
+    pSignal = np.mean(original)
+    pNoise = np.mean((original - compressed) ** 2)
+    if(pNoise == 0):  # MSE is zero means no noise is present in the signal .
+        return "Same Image"
+    pNoise = sqrt(pNoise)
+    # max_pixel = 255.0
+    snr = 10 * log10(pSignal/pNoise)
+    return snr
 
 if (originalImages and nearestNeighbourImages):
     for img in range(len(originalImages)):
@@ -32,5 +33,7 @@ if (originalImages and nearestNeighbourImages):
         image2 = pil.open(pathImage2)
         image1 = np.array(image1)
         image2 = np.array(image2)
+
+        print("Value SNR = ", signal_noise_ratio(image1, image2), "for the Image: ", originalImages[img])
 
 
